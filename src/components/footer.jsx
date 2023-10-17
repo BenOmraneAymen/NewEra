e36be99e92bc1cc9cd467c5c8a45142e7b35a889
Aymen Ben Omrane <aymen.b.omrane@gmail.com>
2023-10-17 23:49:36 +0100

    better footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className="w-screen h-48 lg:h-screen-1/2  bg-back flex flex-col items-center justify-center">
         <div className="w-full flex lg:justify-between gap-x-12">
           <div className="flex justify-center lg:justify-start ">
-            <div className="w-52 flex flex-col p-2 ">
+            <div className="w-48 lg:w-64 flex flex-col p-2 ">
               <div className="text-slate-300 font-extrabold text-xl">
                 NewEra Company
               </div>
@@ -17,7 +17,7 @@ export default function Footer() {
                 Mahdia - Tunisia
               </div>
             </div>
-            <div className="w-52 flex flex-col p-2">
+            <div className="w-48 lg:w-64 flex flex-col p-2">
               <div className="flex items-center text-slate-300 font-medium text-base py-1">
                 <span class="material-symbols-outlined px-1">call</span>
                 &nbsp;+216 98 000 000
